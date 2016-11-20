44ef81da5654428fd0132189af3c9182c63d4365
sviktor <viktor.sanca@gmail.com>
2016-11-21 00:23:06 +0100

    Initial Commit of Spring Boot project.
    Java 1.8
    Spring Boot 1.4.2
    Maven Project

---
+++ FoodBook/src/main/java/e2/isa/grupa5/FoodBookApplication.java
@@ -0,0 +1,12 @@
+package e2.isa.grupa5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBookApplication.class, args);
+	}
+}
