09af3c4459e562ae3f4a5af58d29781cee8997bf
Jelena Jankovic <jelenaa3105@gmail.com>
2017-02-22 22:39:46 +0100

    Uklanjanje globalnih promenljivih. Izolacija koda unutar IIFE-ja. Refaktorisan app.js, uklonjen nekoriscen kod. Vracen DependencyInjection.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,185 +1,120 @@
 /**
- * Created by Viktor on 12/18/2016.
+ * IFFE is used to avoid dirtying global namespace and or potential conflicts.
+ * EACH module, controller, service should be contained within its own scope by wrapping it within an IIFE.
+ * .$inject is used to specify required DependencyInjection; for code to remain functional after minification/obfuscation.
  */
-
-var foodbook = angular.module('foodbook', ['ngRoute', 'cgNotify', 'ui.router', 'ui.bootstrap']);
-
-foodbook.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
-	
-	//$urlRouterProvider.otherwise('/login')
-    $stateProvider
-    	.state('login', {
-    		url: '/login',
-    		controller: 'LoginController',
-    		templateUrl: 'pages/user/login.html',
-    	})
-        .state('register', {
-        	url: '/guest/register',
-        	templateUrl: 'pages/guest/register/registerGuest.html',
-            controller: 'RegisterGuestController'
-        })
-        .state('guest-profile', {
-        	url: '/guest/profile-page',
-        	templateUrl: 'pages/guest/profilePage/profilePage.html',
-            controller: 'ProfilePageController'
-        })
-        .state('guest-confirm', {
-        	url: '/guest/confirm-registration',
-        	templateUrl: 'pages/guest/confirmRegistration/confirmRegistration.html',
-            controller: 'ConfirmRegistrationController'  
-        })
-        .state('guest-home', {
-            url: '/guest/home-page',
-        	templateUrl: 'pages/guest/homePage/homePage.html',
-            controller: 'HomePageController'
-        })
-        .state('guest-friends', {
-            url: '/guest/friends-page',
-        	templateUrl: 'pages/guest/friendsPage/friendsPage.html',
-            controller: 'FriendsPageController'
-        })
-        .state('guest-restaurants', {
-            url: '/guest/restaurants-page',
-        	templateUrl: 'pages/guest/restaurantsPage/restaurantsPage.html',
-            controller: 'RestaurantsPageController'
-        })
-    	.state('sysmanager', {
-    		url: '/sm',
-    		templateUrl: 'pages/sysmanager/profile.html',
-    		controller: 'sysmanagerProfileController'
-    	})
-    	.state('sysmanager.home', {
-    		url: '/panel',
-    		templateUrl: 'pages/sysmanager/home.html',
-    		controller: 'sysmanagerHomeController'
-    	})
-    	.state('sysmanager.addSystemManager', {
-    		url: '/addSystemManager',
-    		templateUrl: 'pages/sysmanager/addSystemManager.html',
-    		controller: 'sysmanagerAddSystemManagerController'
-    	})
-    	.state('sysmanager.addRestaurantManager', {
-    		url: '/addRestaurantManager',
-    		templateUrl: 'pages/sysmanager/addRestaurantManager.html',
-    		controller: 'sysmanagerAddRestaurantManagerController'
-    	})
-    	.state('sysmanager.addRestaurant', {
-    		url: '/addRestaurant',
-    		templateUrl: 'pages/sysmanager/addRestaurant.html',
-    		controller: 'sysmanagerAddRestaurantController'
-    	})
-    	.state('sysmanager.addBidder', {
-    		url: '/addBidder',
-    		templateUrl: 'pages/sysmanager/addBidder.html',
-    		controller: 'sysmanagerAddBidderController'
-    	})
-    	.state('manager', {
-    		url: '/rm',
-    		templateUrl: 'pages/manager/profile.html',
-    		controller: 'managerProfileController'
-    	})
-    	.state('manager.home', {
-    		url: '/panel',
-    		templateUrl: 'pages/manager/home.html',
-    		controller: 'managerHomeController'
-    	})
-    	.state('bidder', {
-    		url: '/bd',
-    		templateUrl: 'pages/bidder/profile.html',
-    		controller: 'bidderProfileController'
-    	})
-    	.state('bidder.home', {
-    		url: 'panel',
-    		templateUrl: 'pages/bidder/home.html',
-    		controller: 'bidderHomeController'
-    	})
-	    .state('bartender', {
-			url: '/bartender',
-			templateUrl: 'pages/bartender/profile.html',
-			controller: 'bartenderProfileController'
-		})
-		.state('bartender.home', {
-			url: 'panel',
-			templateUrl: 'pages/bartender/home.html',
-			controller: 'bartenderHomeController'
-		});
-    	
-
-}).run(function($rootScope, $state, $location, sessionService) {
-    console.log("Application ready to go!");
-    
-    if (sessionService.getUserInfo() === null && $location.path() !== 'pages/guest/confirm-registration') {
-    	$state.go('login');
-    }
-
-    $rootScope.$on("$routeChangeStart", function (event, next, current) {
-        if (sessionService.getUserInfo() === null) {
-            // no logged user, we should be going to #login
-            if (next.templateUrl !== "pages/user/login.html" && next.templateUrl !== "pages/guest/register/registerGuest.html" && next.templateUrl !== "pages/guest/confirmRegistration/confirmRegistration.html") {
-                console.log("Not logged in! Redirecting to login...");
-                // not going to #login, we should redirect now
-                //$location.path("/login");
-                $state.go('login');
-            } 
-        }
-    });
-});
-	
-/*
-
 (function () {
     'use strict';
 
-    angular.module('foodbook', )
-    .config(function ($routeProvider, $stateProvider) {
-    	
-        $routeProvider.otherwise('/login')
+    angular.module('foodbook', ['ngRoute', 'cgNotify', 'ui.router', 'ui.bootstrap']).config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
         $stateProvider
-        	.state('login', {
-        		url: '/login',
-        		controller: 'LoginController',
-        		templateUrl: 'pages/user/login.html'
-        	})
+            .state('login', {
+                url: '/login',
+                controller: 'LoginController',
+                templateUrl: 'pages/user/login.html',
+            })
             .state('register', {
-            	url: '/guest/register',
-            	templateUrl: 'pages/guest/register/registerGuest.html',
+                url: '/guest/register',
+                templateUrl: 'pages/guest/register/registerGuest.html',
                 controller: 'RegisterGuestController'
             })
-            .state('guest.profile', {
-            	url: '/guest/profile-page',
-            	templateUrl: 'pages/guest/profilePage/profilePage.html',
+            .state('guest-profile', {
+                url: '/guest/profile-page',
+                templateUrl: 'pages/guest/profilePage/profilePage.html',
                 controller: 'ProfilePageController'
             })
-            .state('guest.confirm', {
-            	url: '/guest/confirm-registration',
-            	templateUrl: 'pages/guest/confirmRegistration/confirmRegistration.html',
-                controller: 'ConfirmRegistrationController'  
+            .state('guest-confirm', {
+                url: '/guest/confirm-registration',
+                templateUrl: 'pages/guest/confirmRegistration/confirmRegistration.html',
+                controller: 'ConfirmRegistrationController'
             })
-            .state('guest.home', {
+            .state('guest-home', {
                 url: '/guest/home-page',
-            	templateUrl: 'pages/guest/homePage/homePage.html',
+                templateUrl: 'pages/guest/homePage/homePage.html',
                 controller: 'HomePageController'
             })
-            .state('guest.friends', {
+            .state('guest-friends', {
                 url: '/guest/friends-page',
-            	templateUrl: 'pages/guest/friendsPage/friendsPage.html',
+                templateUrl: 'pages/guest/friendsPage/friendsPage.html',
                 controller: 'FriendsPageController'
             })
-            .state('guest.restaurants', {
+            .state('guest-restaurants', {
                 url: '/guest/restaurants-page',
-            	templateUrl: 'pages/guest/restaurantsPage/restaurantsPage.html',
+                templateUrl: 'pages/guest/restaurantsPage/restaurantsPage.html',
                 controller: 'RestaurantsPageController'
+            })
+            .state('sysmanager', {
+                url: '/sm',
+                templateUrl: 'pages/sysmanager/profile.html',
+                controller: 'sysmanagerProfileController'
+            })
+            .state('sysmanager.home', {
+                url: '/panel',
+                templateUrl: 'pages/sysmanager/home.html',
+                controller: 'sysmanagerHomeController'
+            })
+            .state('sysmanager.addSystemManager', {
+                url: '/addSystemManager',
+                templateUrl: 'pages/sysmanager/addSystemManager.html',
+                controller: 'sysmanagerAddSystemManagerController'
+            })
+            .state('sysmanager.addRestaurantManager', {
+                url: '/addRestaurantManager',
+                templateUrl: 'pages/sysmanager/addRestaurantManager.html',
+                controller: 'sysmanagerAddRestaurantManagerController'
+            })
+            .state('sysmanager.addRestaurant', {
+                url: '/addRestaurant',
+                templateUrl: 'pages/sysmanager/addRestaurant.html',
+                controller: 'sysmanagerAddRestaurantController'
+            })
+            .state('sysmanager.addBidder', {
+                url: '/addBidder',
+                templateUrl: 'pages/sysmanager/addBidder.html',
+                controller: 'sysmanagerAddBidderController'
+            })
+            .state('manager', {
+                url: '/rm',
+                templateUrl: 'pages/manager/profile.html',
+                controller: 'managerProfileController'
+            })
+            .state('manager.home', {
+                url: '/panel',
+                templateUrl: 'pages/manager/home.html',
+                controller: 'managerHomeController'
+            })
+            .state('bidder', {
+                url: '/bd',
+                templateUrl: 'pages/bidder/profile.html',
+                controller: 'bidderProfileController'
+            })
+            .state('bidder.home', {
+                url: 'panel',
+                templateUrl: 'pages/bidder/home.html',
+                controller: 'bidderHomeController'
+            })
+            .state('bartender', {
+                url: '/bartender',
+                templateUrl: 'pages/bartender/profile.html',
+                controller: 'bartenderProfileController'
+            })
+            .state('bartender.home', {
+                url: 'panel',
+                templateUrl: 'pages/bartender/home.html',
+                controller: 'bartenderHomeController'
             });
+
 
     }).run(run);
 
-    run.$inject = ["$rootScope", "$location", 'sessionService'];
+    // required DependencyInjection
+    run.$inject = ["$rootScope", "$location", '$state', 'sessionService'];
 
     function run($rootScope, $location, sessionService) {
         console.log("Application ready to go!");
 
         if (sessionService.getUserInfo() === null && $location.path() !== 'pages/guest/confirm-registration') {
-            $location.path("/login");
+            $state.go('login');
         }
 
         $rootScope.$on("$routeChangeStart", function (event, next, current) {
@@ -188,10 +123,10 @@ foodbook.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
                 if (next.templateUrl !== "pages/user/login.html" && next.templateUrl !== "pages/guest/register/registerGuest.html" && next.templateUrl !== "pages/guest/confirmRegistration/confirmRegistration.html") {
                     console.log("Not logged in! Redirecting to login...");
                     // not going to #login, we should redirect now
-                    $location.path("/login");
-                } 
+                    $state.go('login');
+                }
             }
         });
-    }
+    };
 
-//})();*/
+})();
