a26ee75291ff5669438755978bd785c24908bdfb
vsanca <viktor.sanca@gmail.com>
2017-02-23 15:50:25 +0100

    Restaurant manager interface.

--- src/main/resources/static/pages/manager/restaurant.js
+++ src/main/resources/static/pages/manager/restaurant.js
@@ -1,3 +1,30 @@
-angular.module('foodbook').controller('managerRestaurantController', function($scope, $http, $state) {
+angular.module('foodbook').controller('managerRestaurantController', function($scope, $http, $state, sessionService) {
 	
+	$scope.restaurant = {};
+	
+	$http.get('/restaurants/manager/'+sessionService.getUserInfo().userId, 
+			{ headers: { 'Authorization': sessionService.getAuthToken() } })
+			.success(function (data) {
+				$scope.restaurant = data;
+	});
+	
+	//name, description, address, email, phone
+	$scope.modifyRestaurant = function() {
+		$http.post('/restaurants/update', $scope.restaurant,
+				{ headers: { 'Authorization': sessionService.getAuthToken() } })
+				.success(function (data) {
+					
+				})
+				.error(function() {
+					alert('Greška prilikom modifikacije.');
+				});
+	};
+	
+	$scope.cancel = function() {
+		$http.get('/restaurants/manager/'+sessionService.getUserInfo().userId, 
+				{ headers: { 'Authorization': sessionService.getAuthToken() } })
+				.success(function (data) {
+					$scope.restaurant = data;
+		});
+	};
 });
