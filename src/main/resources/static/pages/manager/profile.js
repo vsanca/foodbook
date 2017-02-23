4296766211caa62a84d7a90f66da65bf29f59db9
vsanca <viktor.sanca@gmail.com>
2017-02-23 13:30:36 +0100

    Base profile set for Restaurant manager.

--- src/main/resources/static/pages/manager/profile.js
+++ src/main/resources/static/pages/manager/profile.js
@@ -1,3 +1,41 @@
 angular.module('foodbook').controller('managerProfileController', function($scope, $http, $state) {
 	
+	$state.transitionTo('manager.home');
+	
+	$scope.home = function() {
+        $state.transitionTo('manager.home');
+    };
+
+    $scope.groceries = function() {
+        $state.transitionTo('manager.groceries');
+    };
+    
+    $scope.menu = function() {
+        $state.transitionTo('manager.menu');
+    };
+
+    $scope.registerWorker = function() {
+        $state.transitionTo('manager.registerWorker');
+    };
+    
+    $scope.report = function() {
+        $state.transitionTo('manager.report');
+    };
+    
+    $scope.restaurant = function() {
+        $state.transitionTo('manager.restaurant');
+    };
+    
+    $scope.setLayout = function() {
+        $state.transitionTo('manager.setLayout');
+    };
+    
+    $scope.setTimetable = function() {
+        $state.transitionTo('manager.setTimetable');
+    };
+    
+    $scope.workers = function() {
+        $state.transitionTo('manager.workers');
+    };
+    
 });
