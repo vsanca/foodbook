3e982882fb8358222be5aa4fc0a2bc31af2a3224
vsanca <viktor.sanca@gmail.com>
2017-02-23 12:21:49 +0100

    Login redirect if already logged in.

--- src/main/resources/static/pages/user/LoginController.js
+++ src/main/resources/static/pages/user/LoginController.js
@@ -8,10 +8,35 @@
   'use strict';
 
   angular.module('foodbook').controller('LoginController', LoginController);
-  LoginController.$inject = ['$scope', '$http', 'authenticationService', 'userRoles', '$location', '$state'];
+  LoginController.$inject = ['$scope', '$http', 'authenticationService', 'userRoles', '$location', '$state', 'sessionService'];
 
-  function LoginController($scope, $http, authenticationService, userRoles, $location, $state) {
+  function LoginController($scope, $http, authenticationService, userRoles, $location, $state, sessionService) {
     console.log("LoginController ready!");
+    
+    // možda ovakva provera da li je već ulogovan, pa odgovarajući redirect, isprobano - radi ali zakomentarisano da bi se moglo lakše testirati
+    /*
+    if(sessionService.getUserInfo() !== null) {
+    	var role = sessionService.getUserInfo().role
+    	
+    	if (role === userRoles["GUEST"]) {
+        	$state.go('guest-profile')
+        } else if (role === userRoles['CHEF']) {
+        	$state.go('chef')
+        } else if (role === userRoles['WAITER']) {
+        	$state.go('waiter')
+        } else if (role === userRoles['MANAGER']) {
+        	$state.go('manager');
+        } else if (role === userRoles['BIDDER']) {
+        	$state.go('bidder');
+        } else if (role === userRoles['BARTENDER']) {
+        	$state.go('bartender')
+        } else if (role === userRoles['SYS_MANAGER']) {
+        	$state.go('sysmanager');
+        }
+    	
+    }
+    */
+    
     $scope.user = {
       username: null,
       password: null
