08f998ac2b78dcf04bd80d5e80445d61566ac1fc
vsanca <viktor.sanca@gmail.com>
2017-02-26 00:49:29 +0100

    Bidder profile update to facilitate modal dialogs (Bootstrap-ui bug).

--- src/main/resources/static/pages/bidder/profile.js
+++ src/main/resources/static/pages/bidder/profile.js
@@ -3,18 +3,26 @@ angular.module('foodbook').controller('bidderProfileController', function($scope
 	$state.transitionTo('bidder.home');
 	
 	$scope.home = function() {
+		$(".nav").find(".active").removeClass("active");
+		$('#li1').addClass('active');
         $state.transitionTo('bidder.home');
     };
 
     $scope.overview = function() {
+    	$(".nav").find(".active").removeClass("active");
+    	$('#li3').addClass('active');
         $state.transitionTo('bidder.overview');
     };
     
     $scope.biddings = function() {
+    	$(".nav").find(".active").removeClass("active");
+    	$('#li4').addClass('active');
         $state.transitionTo('bidder.biddings');
     };
 
     $scope.newBidding = function() {
+    	$(".nav").find(".active").removeClass("active");
+    	$('#li2').addClass('active');
         $state.transitionTo('bidder.newBidding');
     };
 	
