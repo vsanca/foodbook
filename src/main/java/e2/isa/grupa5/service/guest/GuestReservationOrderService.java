a51d11be20764c893ed43f7be3982347736299e9
Juri <Juri@Juri-PC>
2017-02-28 23:28:11 +0100

    Order,ReservationTable,InvitedToReservation services stub.

--- src/main/java/e2/isa/grupa5/service/guest/GuestReservationOrderService.java
+++ src/main/java/e2/isa/grupa5/service/guest/GuestReservationOrderService.java
@@ -3,6 +3,6 @@ package e2.isa.grupa5.service.guest;
 import org.springframework.stereotype.Service;
 
 @Service
-public class InvitedToReservationService {
+public class GuestReservationOrderService {
 
 }
