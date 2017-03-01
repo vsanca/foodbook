944660feac4c81ba22fb77f59fbae2c1378c8c95
vsanca <viktor.sanca@gmail.com>
2017-03-01 13:59:29 +0100

    Unit testing.

--- src/test/java/e2/isa/grupa5/service/restaurant/RestaurantManagerServiceTest.java
+++ src/test/java/e2/isa/grupa5/service/restaurant/RestaurantManagerServiceTest.java
@@ -8,13 +8,30 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import e2.isa.grupa5.model.restaurant.Restaurant;
+import e2.isa.grupa5.model.users.RestaurantManager;
+import e2.isa.grupa5.repository.restaurant.RestaurantManagerRepository;
+import e2.isa.grupa5.repository.restaurant.RestaurantRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RestaurantManagerServiceTest {
-
+	
+	@Autowired
+	private RestaurantManagerService rmService;
+	
+	@Autowired
+	private RestaurantRepository rRepo;
+	
+	@Autowired
+	private RestaurantManagerRepository rmRepo;
+	
+	private long mId;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -25,15 +42,47 @@ public class RestaurantManagerServiceTest {
 
 	@Before
 	public void setUp() throws Exception {
+		
+		Restaurant r = new Restaurant();
+		r.setAddress("address");
+		r.setDescription("description");
+		r.setEmail("email");
+		r.setName("name");
+		r.setPhone("12345");
+		
+		r = rRepo.save(r);
+		
+		RestaurantManager rm = new RestaurantManager();
+		rm.setActive(true);
+		rm.setAddress("address");
+		rm.setEmail("email");
+		rm.setName("name");
+		rm.setSurname("surname");
+		rm.setPassword("password");
+		rm.setRestaurant(r);
+		
+		rm = rmRepo.save(rm);
+		
+		mId = rm.getId();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
+		rmRepo.deleteAll();
+		rRepo.deleteAll();
+		
 	}
 
 	@Test
 	public void testFindById() {
-		fail("Not yet implemented");
+		
+		RestaurantManager rm = rmService.findById(mId);
+		
+		assertNotNull(rm);
+		assertEquals(rm.getName(), "name");
+		assertEquals(rm.getAddress(), "address");
+		
 	}
 
 	@Test
