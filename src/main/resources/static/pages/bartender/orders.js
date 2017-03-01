a274263bc6834804cf2c9dcdc3a3d6e901464203
Juri <Juri@Juri-PC>
2017-03-01 23:14:53 +0100

    Waiter order interface.

--- src/main/resources/static/pages/bartender/orders.js
+++ src/main/resources/static/pages/bartender/orders.js
@@ -1,3 +1,76 @@
-angular.module('foodbook').controller('bartenderOrdersController', function($scope, $http, $state) {
-	$scope.activePageNumber = 2;
+angular.module('foodbook').controller('bartenderOrdersController', function($scope, $http, $state,$location, sessionService , notifyService,authenticationService) {
+	
+$scope.activePageNumber = 2;
+	
+	
+	
+	$http.get('/user/bartender/profile/'+sessionService.getUserInfo().userId, 
+			{ headers: { 'Authorization': sessionService.getAuthToken() } })
+			.success(function (data) {
+				$scope.chef = data;
+				$scope.chef.id = sessionService.getUserInfo().userId;
+				
+				/*$http.get('/user/chef/allUnfinishedOdersTEST/'+sessionService.getUserInfo().userId, 
+						{ headers: { 'Authorization': sessionService.getAuthToken() } })
+						
+									.success(function (data) {
+							
+										console.log(data);
+										$scope.myOrdersDTO = data;
+							
+							
+							
+						});*/
+				
+				
+	});
+	
+	
+	/*
+	$http.get('/user/chef/allUnfinishedOdersTEST/'+sessionService.getUserInfo().userId, 
+			{ headers: { 'Authorization': sessionService.getAuthToken() } })
+			
+						.success(function (data) {
+				
+							console.log(data);
+							$scope.myOrdersDTO = data;
+				
+				
+				
+			});
+	
+	
+	
+	
+	$scope.acceptOrder = function(o){
+		
+		$http.get('/user/chef/acceptedOrder/'+ o.myId +'/'+ sessionService.getUserInfo().userId, 
+				{ headers: { 'Authorization': sessionService.getAuthToken() } })
+				.success(function (data) {
+					
+					$scope.myOrdersDTO = data;
+					
+					
+					notifyService.showSuccess('Porudžbina prihvaćena!');
+				})
+				.error(function() {
+					notifyService.showError('Porudžbina nije uspešno prihvaćena!');
+				});
+	}
+	
+	$scope.createOrder = function(o){
+		$http.get('/user/chef/createdOrder/'+ o.myId, 
+				{ headers: { 'Authorization': sessionService.getAuthToken() } })
+				.success(function (data) {
+					
+					$scope.myOrdersDTO = data;
+					
+					
+					notifyService.showSuccess('Uspešno sačuvano: obrok napravljen!');
+				})
+				.error(function() {
+					notifyService.showError('Nije uspešno sačuvano: obrok nije napravljen!!!');
+				});
+	}
+	*/
 });
