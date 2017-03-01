bea6e21b8ab4534653153a8da721b1ef3377dda3
vsanca <viktor.sanca@gmail.com>
2017-03-01 12:35:20 +0100

    Unit testing.

--- src/test/java/e2/isa/grupa5/service/restaurant/RestaurantManagerServiceTest.java
+++ src/test/java/e2/isa/grupa5/service/restaurant/RestaurantManagerServiceTest.java
@@ -7,7 +7,12 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class RestaurantManagerServiceTest {
 
 	@BeforeClass
